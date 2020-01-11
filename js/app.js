82286487a33ddcf69a19530f8e7249f68884022f
Aman mool <amanmool384@gmail.com>
2020-01-11 21:43:00 +0545

    completing team section

--- js/app.js
+++ js/app.js
@@ -1,108 +1,117 @@
 // preloader
-$(window).on('load',function(){
+$(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(300).fadeOut('slow');
 });
 
 //home type writer testing
-const TypeWriter = function(txtElement, words ,wait =3000)
-{
-    this.txtElement=txtElement;
-    this.words=words;
-    this.txt= '';
-    this.wordIndex =0;
-    this.wait = parseInt(wait , 10);
+const TypeWriter = function (txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
     this.type();
-    this.isDeleting =false;
+    this.isDeleting = false;
 }
 
 //type method
-TypeWriter.prototype.type = function()
-{
+TypeWriter.prototype.type = function () {
     //current index of word
     const current = this.wordIndex % this.words.length;
     //get full text of current word
-    const fullTxt=this.words[current];
+    const fullTxt = this.words[current];
 
     //check if deleting
-    if(this.isDeleting)
-    {
+    if (this.isDeleting) {
         //Remove char
-        this.txt=fullTxt.substring(0, this.txt.length - 1);
-    }
-    else{
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
         //Add char
-        this.txt = fullTxt.substring(0,this.txt.length+1);
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
     //Insert txt into element
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     //type speed
-    let typeSpeed =300;
+    let typeSpeed = 300;
 
-    if(this.isDeleting){
-        typeSpeed/=2;
+    if (this.isDeleting) {
+        typeSpeed /= 2;
     }
-    
+
     //If word is completing bhanne chai change garna ko lagi
 
-    if(!this.isDeleting&& this.txt=== fullTxt)
-    {
+    if (!this.isDeleting && this.txt === fullTxt) {
         //to make a pause at the end
-        typeSpeed=this.wait;
+        typeSpeed = this.wait;
         //set delete to the true
         this.isDeleting = true;
 
-    }
-    else if(this.isDeleting && this.txt === '')
-    {
+    } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         //move to next
         this.wordIndex++;
         //pause before start typing next
-        typeSpeed= 500;
+        typeSpeed = 500;
     }
 
     console.log(this.txt);
     setTimeout(() => this.type(), typeSpeed);
 }
 //init in dom load
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init);
 
 //init function
-function init()
-{
-    const txtElement=document.querySelector('.txt-type');
+function init() {
+    const txtElement = document.querySelector('.txt-type');
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
 
     //Init typewriter
-    new  TypeWriter(txtElement,words,wait);
+    new TypeWriter(txtElement, words, wait);
 }
 
-
-
 /*
-   ****************************************Team********************
-*/
+ ****************************************Team********************
+ */
 
 // short form for document.ready method which takes all the javascript functions
 
- $(function()
- {
+$(function () {
 
     $("#team-members").owlCarousel({
         items: 2,
         autoplay: true,
         smartSpeed: 500,
         loop: true,
-        autoplayHoverPause:true,
+        autoplayHoverPause: true,
         nav: true,
-        dots:false,
-        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
 
 
     });
 
- });
+});
+
+/*
+ ********************************progress bars
+ */
+
+$(function () {
+    $("#team #progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+});
