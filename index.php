2aab7f1969e70155647dbeb3e4cb058dd09dda72
Aman mool <amanmool384@gmail.com>
2020-08-31 10:02:09 +0545

    updateddd

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- title -->
-    <title>Hamro Gas</title>
+    <title>Hamro Gas FInal</title>
 
     <!-- favIcon -->
     <link rel="shortcut icon" href="images/favicon/burn.png" />
